b2f7f8c5da826ff17c1fa88819fbc1919640e740
Poorjoy <madison_shepard@mail.ru>
2020-03-14 16:23:00 +0300

    feat: added toggle iPhone screen functional

--- script.js
+++ script.js
@@ -24,3 +24,44 @@ menuLinks.forEach(function (item) {
         });
     });
 });
+
+// Black screen
+
+let iphoneVerticalScreen = document.querySelector('.slider-wrap__screen-wrap-vertical'),
+    iphoneHorizontalScreen = document.querySelector('.slider-wrap__screen-wrap-horizontal'),
+    iphoneVerticalScreenOne = document.querySelector('.slider-wrap__screen-wrap-vertical-one'),
+    iphoneVerticalScreenTwo = document.querySelector('.slider-wrap__screen-wrap-vertical-two'),
+    iphoneVerticalScreenThree = document.querySelector('.slider-wrap__screen-wrap-vertical-three'),
+    iphoneBlackVertical = document.querySelector('.slider-wrap__black-vertical'),
+    iphoneBlackHorizontal = document.querySelector('.slider-wrap__black-horizontal'),
+    iphoneBlackVerticalOne = document.querySelector('.slider-wrap__black-vertical-one'),
+    iphoneBlackVerticalTwo = document.querySelector('.slider-wrap__black-vertical-two'),
+    iphoneBlackVerticalThree = document.querySelector('.slider-wrap__black-vertical-three');
+
+function toggleScreen(screen, blackScreen) {
+    screen.addEventListener('click', function () {
+        if (blackScreen.classList.contains('none')) {
+            blackScreen.classList.remove('none');
+        } else blackScreen.classList.add('none');
+    });
+}
+
+// Vertical IPhone (slider 1)
+
+toggleScreen(iphoneVerticalScreen, iphoneBlackVertical);
+
+// Horizontal IPhone (slider 1)
+
+toggleScreen(iphoneHorizontalScreen, iphoneBlackHorizontal);
+
+// Vertical IPhone one (slider 2)
+
+toggleScreen(iphoneVerticalScreenOne, iphoneBlackVerticalOne);
+
+// Vertical IPhone two (slider 2)
+
+toggleScreen(iphoneVerticalScreenTwo, iphoneBlackVerticalTwo);
+
+// Vertical IPhone three (slider 2)
+
+toggleScreen(iphoneVerticalScreenThree, iphoneBlackVerticalThree);
