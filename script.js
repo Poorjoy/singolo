f2a6a7e8dcd0e4781fe667b2345e2e30fa88b3eb
Poorjoy <madison_shepard@mail.ru>
2020-03-14 16:24:11 +0300

    feat: added slider functional

--- script.js
+++ script.js
@@ -65,3 +65,50 @@ toggleScreen(iphoneVerticalScreenTwo, iphoneBlackVerticalTwo);
 // Vertical IPhone three (slider 2)
 
 toggleScreen(iphoneVerticalScreenThree, iphoneBlackVerticalThree);
+
+// Slider
+
+let slideWidth = 1020,
+    sliderList = document.querySelector('.slider-wrap__list'),
+    slides = document.querySelectorAll('.slider-wrap__slider'),
+    prev = document.querySelector('.slider-links__arrow-prev'),
+    next = document.querySelector('.slider-links__arrow-next'),
+    pos = 0;
+
+sliderList.style.width = slides.length * slideWidth + 'px';
+
+prev.addEventListener('click', scrollToPrev);
+next.addEventListener('click', scrollToNext);
+
+function scrollToPrev() {
+    pos--;
+
+    if (pos < 0) {
+        let children = sliderList.children;
+        sliderList.style.transition = null;
+        sliderList.style.left = -(pos + 2) * slideWidth + 'px';
+        sliderList.insertBefore(children[slides.length - 1], children[0]);
+        children[0].offsetParent;
+        pos++;
+    }
+
+    sliderList.style.transition = 'left 0.6s ease-in-out';
+    sliderList.style.left = -(slideWidth * pos) + 'px';
+}
+
+
+function scrollToNext() {
+    pos++;
+
+    if (pos > slides.length - 1) {
+        let children = sliderList.children;
+        sliderList.style.transition = null;
+        sliderList.style.left = -(pos - 2) * slideWidth + 'px';
+        sliderList.appendChild(children[0]);
+        children[0].offsetParent;
+        pos--;
+    }
+
+    sliderList.style.transition = 'left 0.6s ease-in-out';
+    sliderList.style.left = -(slideWidth * pos) + 'px';
+}
